d2fbd2ea119ea39c57e6df122aa218e6b615e7e7
KevinMallon <mallon.kevin@gmail.com>
2013-07-18 17:15:09 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArtGallery::Application.config.secret_token = 'b5a636553791568ae82941dccb8527b74ca7b58fa4b4290b26282d20e673b94168cc1e1c1d49535ec3b6d42d8c177545193ed2cb0f447aaa06e331799ea0d5f7'
